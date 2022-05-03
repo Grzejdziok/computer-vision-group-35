662bcc4389efa94b9ba191d07dce7d090e8bbf8a
Michał Grzejdziak <mgrzejdziak@gmail.com>
2022-05-03 13:29:54 +0200

    Iterate on VAEEndToEnd

--- models/vae_utils.py
+++ models/vae_utils.py
@@ -1,9 +1,7 @@
-from typing import Tuple, List
+from typing import List, Tuple
+
 import torch
-from torch import nn
-import pytorch_lightning as pl
-import torch.nn.functional as F
-from data.training_sample import TrainingSample, ModelOutput
+import torch.nn as nn
 
 
 class Encoder(nn.Module):
@@ -60,44 +58,3 @@ class Decoder(nn.Module):
         image = self.image_head(features).view(-1, self.s_img, self.s_img, 3)
         mask = self.mask_head(features).view(-1, self.s_img, self.s_img)
         return image, mask
-
-
-class LitVAE(pl.LightningModule):
-    def __init__(self, latent_dims, s_img, hdim):
-        super().__init__()
-        self.encoder = Encoder(latent_dims, s_img, hdim)
-        self.decoder = Decoder(latent_dims, s_img, hdim)
-
-    def _encoder_decoder_forward(self, rgb: torch.FloatTensor) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
-        z = self.encoder(rgb)
-        image, mask = self.decoder(z)
-        return image, mask
-
-    def forward(self, batch: TrainingSample) -> ModelOutput:
-        rgb = batch["model_input"]["rgb"]
-        image, mask_logits = self._encoder_decoder_forward(rgb)
-        soft_object_mask = torch.sigmoid(mask_logits).float()
-        model_outputs = ModelOutput(rgb_with_object=image, soft_object_mask=soft_object_mask,)
-        return model_outputs
-
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
-
-    def training_step(self, batch: TrainingSample, batch_idx: int) -> nn.Module:
-        rgb = batch["model_input"]["rgb"]
-        model_targets = batch["model_target"]
-        image, mask_logits = self._encoder_decoder_forward(rgb=rgb)
-        mask_cross_entropy_loss = F.binary_cross_entropy_with_logits(
-            input=mask_logits,
-            target=model_targets["object_mask"].float(),
-        )
-        rgb_mse_loss = F.mse_loss(
-            input=image,
-            target=model_targets["rgb_with_object"],
-        )
-        loss = mask_cross_entropy_loss + rgb_mse_loss + self.encoder.kl
-        self.log("ce_loss", mask_cross_entropy_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log("mse_loss", rgb_mse_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log("kl_loss", self.encoder.kl, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss

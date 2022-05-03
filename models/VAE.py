Make feature extractors in VAE common for all heads and fix KL divergence

models/VAE.py
@@ -1,5 +1,4 @@
-from statistics import mode
-from typing import Tuple
+from typing import Tuple, List
 import torch
 from torch import nn
 import pytorch_lightning as pl
@@ -8,78 +7,58 @@ from data.training_sample import TrainingSample, ModelOutput
 
 
 class Encoder(nn.Module):
-    def __init__(self, latent_dims: int, s_img: int, hdim: Tuple[int, int]):
+    def __init__(self, latent_dims: int, s_img: int, hdim: List[int]):
         super(Encoder, self).__init__()
-        
-        self.mean_layers = nn.Sequential(
-            nn.Linear(s_img*s_img*3, hdim[0]),
-            nn.ReLU(),
-            nn.Linear(hdim[0], hdim[1]),
-            nn.ReLU(),
-            nn.Linear(hdim[1], latent_dims)
-        )
-        self.sigma_layers = nn.Sequential(
-            nn.Linear(s_img*s_img*3, hdim[0]),
-            nn.ReLU(),
-            nn.Linear(hdim[0], hdim[1]),
-            nn.ReLU(),
-            nn.Linear(hdim[1], latent_dims),
-        )
+
+        input_dim = s_img**2*3
+        common_layers = [nn.Flatten()]
+        for i, h in enumerate(hdim):
+            common_layers.append(nn.Linear(hdim[i-1] if i > 0 else input_dim, h))
+            common_layers.append(nn.ReLU())
+
+        self.feature_extractor = nn.Sequential(*common_layers)
+        self.mean_head = nn.Linear(hdim[-1], latent_dims)
+        self.sigma_head = nn.Linear(hdim[-1], latent_dims)
 
         #distribution setup
         self.N = torch.distributions.Normal(0, 1)
         self.kl = 0
 
-    def kull_leib(self, mu: torch.Tensor, sigma: torch.Tensor) -> torch.Tensor:
-        return (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+    def kull_leib(self, mu: torch.Tensor, sig: torch.Tensor) -> torch.Tensor:
+        return torch.distributions.kl.kl_divergence(torch.distributions.Normal(mu, sig), self.N)[:, 0].sum()
 
     def reparameterize(self, mu: torch.Tensor, sig: torch.Tensor) -> torch.Tensor:
         return mu + sig*self.N.sample(mu.shape).to(mu.device)
 
     def forward(self, rgb: torch.Tensor) -> torch.Tensor:
-
-        x = rgb.flatten(start_dim=1)
-
-        sig = self.sigma_layers(x)
-        sig = torch.exp(sig) #because it has to stay positive
-        mu = self.mean_layers(x)
-        
+        features = self.feature_extractor(rgb)
+        sig = torch.exp(self.sigma_head(features))  # make it stay positive
+        mu = self.mean_head(features)
         #reparameterize to find z
         z = self.reparameterize(mu, sig)
-
         #loss between N(0,I) and learned distribution
         self.kl = self.kull_leib(mu, sig)
-
         return z
 
 
 class Decoder(nn.Module):
-    def __init__(self, latent_dims: int, s_img: int, hdim: Tuple[int, int]):
+    def __init__(self, latent_dims: int, s_img: int, hdim: List[int]):
         super(Decoder, self).__init__()
-        
-        self.s_img=s_img
-        self.image_layers = nn.Sequential(
-            nn.Linear(latent_dims, hdim[1]),
-            nn.ReLU(),
-            nn.Linear(hdim[1], hdim[0]),
-            nn.ReLU(),
-            nn.Linear(hdim[0], s_img*s_img*3),
-        )
-        self.mask_layers = nn.Sequential(
-            nn.Linear(latent_dims, hdim[1]),
-            nn.ReLU(),
-            nn.Linear(hdim[1], hdim[0]),
-            nn.ReLU(),
-            nn.Linear(hdim[0], s_img*s_img),
-        )
+
+        common_layers = []
+        for i, h in enumerate(hdim[::-1]):
+            common_layers.append(nn.Linear(hdim[-i+1] if i > 0 else latent_dims, h))
+            common_layers.append(nn.ReLU())
+
+        self.feature_extractor = nn.Sequential(*common_layers)
+        self.image_head = nn.Linear(hdim[0], s_img**2*3)
+        self.mask_head = nn.Linear(hdim[0], s_img**2)
+        self.s_img = s_img
 
     def forward(self, z: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-    
-        image = self.image_layers(z)
-        image = image.reshape((-1, self.s_img, self.s_img, 3))
-        mask = self.mask_layers(z)
-        mask = mask.reshape((-1, self.s_img, self.s_img))
-
+        features = self.feature_extractor(z)
+        image = self.image_head(features).view(-1, self.s_img, self.s_img, 3)
+        mask = self.mask_head(features).view(-1, self.s_img, self.s_img)
         return image, mask
 
 
@@ -96,8 +75,9 @@ class LitVAE(pl.LightningModule):
 
     def forward(self, batch: TrainingSample) -> ModelOutput:
         rgb = batch["model_input"]["rgb"]
-        image, mask = self._encoder_decoder_forward(rgb)
-        model_outputs = ModelOutput(rgb_with_object=image, soft_object_mask=torch.sigmoid(mask).float(),)
+        image, mask_logits = self._encoder_decoder_forward(rgb)
+        soft_object_mask = torch.sigmoid(mask_logits).float()
+        model_outputs = ModelOutput(rgb_with_object=image, soft_object_mask=soft_object_mask,)
         return model_outputs
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
@@ -107,17 +87,17 @@ class LitVAE(pl.LightningModule):
     def training_step(self, batch: TrainingSample, batch_idx: int) -> nn.Module:
         rgb = batch["model_input"]["rgb"]
         model_targets = batch["model_target"]
-        image, mask = self._encoder_decoder_forward(rgb=rgb)
+        image, mask_logits = self._encoder_decoder_forward(rgb=rgb)
         mask_cross_entropy_loss = F.binary_cross_entropy_with_logits(
-            input=mask,
-            target=model_targets["object_mask"].float()
+            input=mask_logits,
+            target=model_targets["object_mask"].float(),
         )
         rgb_mse_loss = F.mse_loss(
             input=image,
             target=model_targets["rgb_with_object"],
         )
-        loss = mask_cross_entropy_loss + rgb_mse_loss + self.encoder.kl*0.1
+        loss = mask_cross_entropy_loss + rgb_mse_loss + self.encoder.kl
         self.log("ce_loss", mask_cross_entropy_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("mse_loss", rgb_mse_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log("kl_loss", self.encoder.kl*0.1, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("kl_loss", self.encoder.kl, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
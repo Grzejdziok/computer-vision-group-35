bd5cf5ae8591ecd3d6a916eacd8e1d316c28d688
Michał Grzejdziak <mgrzejdziak@gmail.com>
2022-05-25 17:58:50 +0200

    Fix and clean-up vae_end_to_end

--- models/vae_utils.py
+++ models/vae_utils.py
@@ -2,6 +2,7 @@ from abc import ABC
 from typing import List, Tuple
 import torch
 import torch.nn as nn
+import torchvision.transforms.functional as TF
 
 
 class Encoder(ABC, nn.Module):
@@ -22,13 +23,14 @@ class Decoder(ABC, nn.Module):
 
 class EncoderFullyConnected(Encoder):
 
-    def __init__(self, latent_dims: int, s_img: int, hdim: List[int]):
+    def __init__(self, latent_dims: int, input_image_size: int, hdim: List[int]):
         super(EncoderFullyConnected, self).__init__()
 
-        input_dim = s_img**2*4
+        self.input_image_size = input_image_size
+        input_dim = input_image_size**2*4
         common_layers = [nn.Flatten()]
         for h in hdim:
-            common_layers.append(nn.Linear(hdim[i-1] if i > 0 else input_dim, h))
+            common_layers.append(nn.Linear(input_dim, h))
             common_layers.append(nn.ReLU())
             input_dim = h
 
@@ -54,8 +56,11 @@ class EncoderFullyConnected(Encoder):
         return torch.distributions.kl.kl_divergence(torch.distributions.Normal(mu, sig), self.N).mean()
 
     def forward(self, rgb_with_object: torch.Tensor, object_mask: torch.Tensor) -> torch.Tensor:
-        rgb_with_object_flat = torch.flatten(rgb_with_object, start_dim=1)
-        object_mask_flat = torch.flatten(object_mask.float(), start_dim=1)
+        rgb_with_object_resized = TF.resize(rgb_with_object, (self.input_image_size, self.input_image_size))
+        object_mask_resized = TF.resize(object_mask, (self.input_image_size, self.input_image_size))
+
+        rgb_with_object_flat = torch.flatten(rgb_with_object_resized, start_dim=1)
+        object_mask_flat = torch.flatten(object_mask_resized.float(), start_dim=1)
         encoder_input = torch.concat([rgb_with_object_flat, object_mask_flat], dim=1)
         features = self.feature_extractor(encoder_input)
         sig = torch.exp(self.sigma_head(features))  # make it stay positive
@@ -68,10 +73,10 @@ class EncoderFullyConnected(Encoder):
 
 
 class DecoderFullyConnected(Decoder):
-    def __init__(self, latent_dims: int, s_img: int, hdim: List[int]):
+    def __init__(self, latent_dims: int, model_output_image_size: int, output_image_size: int, hdim: List[int]):
         super(DecoderFullyConnected, self).__init__()
 
-        rgb_embedding_dims = s_img
+        rgb_embedding_dims = model_output_image_size
         common_layers = []
         in_features = latent_dims + rgb_embedding_dims
         for h in hdim[::-1]:
@@ -80,21 +85,26 @@ class DecoderFullyConnected(Decoder):
             in_features = h
 
         self.rgb_embedding = nn.Sequential(
-            nn.Linear(s_img**2*3, rgb_embedding_dims**2),
+            nn.Linear(output_image_size**2*3, rgb_embedding_dims**2),
             nn.ReLU(),
             nn.Linear(rgb_embedding_dims**2, rgb_embedding_dims),
         )
         self.feature_extractor = nn.Sequential(*common_layers)
-        self.image_head = nn.Linear(in_features, s_img**2*3)
-        self.mask_head = nn.Linear(in_features, s_img**2)
-        self.s_img = s_img
+        self.image_head = nn.Linear(in_features, model_output_image_size**2*3)
+        self.mask_head = nn.Linear(in_features, model_output_image_size**2)
+        self.model_output_image_size = model_output_image_size
+        self.output_image_size = output_image_size
 
     def forward(self, z: torch.Tensor, normalized_rgb: torch.Tensor, rgb: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         normalized_rgb_flat = torch.flatten(normalized_rgb, start_dim=1)
         rgb_embedded = self.rgb_embedding(normalized_rgb_flat)
         features = self.feature_extractor(torch.concat([z, rgb_embedded], dim=1))
-        mask_logits = self.mask_head(features).view(-1, self.s_img, self.s_img)
+        mask_logits = self.mask_head(features).view(-1, self.model_output_image_size, self.model_output_image_size)
         masks = (torch.sigmoid(mask_logits).unsqueeze(1).detach() > 0.5).float()
-        image = self.image_head(features).view(-1, 3, self.s_img, self.s_img) * masks + rgb * (1.-masks)
+        image = self.image_head(features).view(-1, 3, self.model_output_image_size, self.model_output_image_size) * masks \
+                + rgb * (1.-masks)
+
+        mask_logits = TF.resize(mask_logits, (self.output_image_size, self.output_image_size))
+        image = TF.resize(image, (self.output_image_size, self.output_image_size))
 
         return image, mask_logits
